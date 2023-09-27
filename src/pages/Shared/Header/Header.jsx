7c0e2d811c33f1dfe37a34173618808a6b59455f
hossainta1 <tanvir.hossain303030@gmail.com>
2023-09-27 22:36:52 +0600

    Create Nav bar

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
-
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
     return (
-        <div>
+        <Container>
             <div className="text-center">
                 <img src={logo} alt="" />
                 <p className='text-secondary'><small>Journalism Without Fear or Favour</small></p>
                 <p>{moment().format("dddd, MMMM Do, YYYY")}</p>
             </div>
-        </div>
+            <div className='d-flex'>
+                <Button variant="danger">Latest</Button>
+                <Marquee className='text-danger' speed={50}>
+                    I can be a React component, multiple React components, or just some text......  I can be a React component, multiple React components, or just some text......
+                </Marquee>
+            </div>
+
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">More deets</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                <Button variant="secondary">Login</Button>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Container>
     );
 };
 
